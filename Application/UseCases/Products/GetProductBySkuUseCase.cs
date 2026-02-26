976145fee9e33fb6ca7b3b6f53662d4843cd0cc6
david <david0119@outlook.es>
2026-02-26 14:43:58 -0600

    feat: application created product use case and dtos

---
+++ Application/UseCases/Products/GetProductBySkuUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+
+
+namespace Application.UseCases.Products
+{
+    public class GetProductBySkuUseCase
+    {
+        private readonly ISkuRepository<ProductEntity> _repository;
+
+        public GetProductBySkuUseCase(ISkuRepository<ProductEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ProductEntity?> ExecuteAsync(string sku)
+        {
+            var product = await _repository.GetBySkuAsync(sku);
+            if (product == null)
+            {
+                throw new InvalidOperationException($"No se encontró un producto con el sku: {sku}");
+            }
+            return product;
+        }
+    }
+}
